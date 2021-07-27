5126f0557a9741f3025fefd7b260a99cc472bd04
Cameron Winston <cameronwinston0413@gmail.com>
2021-07-27 20:50:24 +0000

    Style Fixed

--- audio.py
+++ audio.py
@@ -1,9 +1,11 @@
 import speech_recognition as sr
 from os import path
-                
+
+
 def printWAV(FILE_NAME, pos, clip):
     # use the audio file as the audio source
-    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), 'static/'+FILE_NAME)
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)),
+                           'static/'+FILE_NAME)
     r = sr.Recognizer()
     text = ""
     with sr.AudioFile(AUDIO_FILE) as source:
@@ -14,5 +16,5 @@ def printWAV(FILE_NAME, pos, clip):
         except sr.UnknownValueError:
             text += "Could not understand audio\n"
         except sr.RequestError as e:
-            text += "Could not request results; {0}".format(e)+ "\n"
+            text += "Could not request results; {0}".format(e) + "\n"
     return text
--- web.py
+++ web.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, url_for, flash, redirect, session, g, after_this_request, request
+from flask import Flask, render_template, url_for, flash, redirect
+from flask import session, g, after_this_request, request
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 from audio import printWAV
-import time, random, threading
+import time
+import random
+import threading
 from turbo_flask import Turbo
 from flask_behind_proxy import FlaskBehindProxy
 
-app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+# this gets the name of the file so Flask knows it's name
+app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'c3eec5c8ffb8f4c3b45f24e2b11bf875'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,24 +28,32 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
-interval=20
+
+interval = 20
 FILE_NAME = "TTC.wav"
 turbo = Turbo(app)
 
-@app.route("/")                          # this tells you the URL the method below is related to
+
+# this tells you the URL the method below is related to
+@app.route("/")
 def home():
-    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    return render_template('home.html', subtitle='Home Page',
+                           text='This is the home page')
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html', subtitle='About Page', text='This is the about page')
+    return render_template('about.html', subtitle='About Page',
+                           text='This is the about page')
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit(): # checks if entries are valid
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+    if form.validate_on_submit():  # checks if entries are valid
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=form.password.data)
         try:
             db.session.add(user)
             db.session.commit()
@@ -50,16 +63,18 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route("/captions")
 def captions():
     TITLE = "HOW SCHOOL MAKES KIDS LESS INTELLIGENT | EDDY ZHONG"
     return render_template('captions.html', songName=TITLE, file=FILE_NAME)
 
+
 @app.before_first_request
 def before_first_request():
-    #resetting time stamp file to 0
+    # resetting time stamp file to 0
     try:
-        file = open("pos.txt","w")
+        file = open("pos.txt", "w")
     except fileOpenException:
         print("pos.txt could no be opened")
     else:
@@ -67,14 +82,15 @@ def before_first_request():
     finally:
         file.close()
 
-    #starting thread that will time updates
+    # starting thread that will time updates
     threading.Thread(target=update_captions, daemon=True).start()
+
 
 @app.context_processor
 def inject_load():
     # getting previous time stamp
     try:
-        file = open("pos.txt","r")
+        file = open("pos.txt", "r")
     except fileOpenException:
         print("pos.txt could no be opened")
     else:
@@ -83,7 +99,7 @@ def inject_load():
         file.close()
     # writing next time stamp
     try:
-        file = open("pos.txt","w")
+        file = open("pos.txt", "w")
     except fileOpenException:
         print("pos.txt could no be opened")
     else:
@@ -91,8 +107,9 @@ def inject_load():
     finally:
         file.close()
 
-    #returning captions
-    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+    # returning captions
+    return {'caption': printWAV(FILE_NAME, pos=pos, clip=interval)}
+
 
 def update_captions():
     with app.app_context():
@@ -100,7 +117,9 @@ def update_captions():
             # timing thread waiting for the interval
             time.sleep(interval)
             # forcefully updating captionsPane with caption
-            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+            turbo.push(turbo.replace(render_template('captionsPane.html'),
+                                     'load'))
+
 
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0", use_reloader=False)
